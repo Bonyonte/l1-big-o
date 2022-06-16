621eaf3c43c34bb1b0ee13e99306bddb1726b117
Ian Okumu <okumu.otsembo@gmail.com>
2022-06-16 11:56:34 +0300

    LogTime: Completed

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 const print = (value) => console.log(value)
 
 // user scores
-const scores = [99 ,80, 77, 86, 76, 50, 33, 76, 90, 54, 66, 76]
+const scores = [99 ,4567 , 80, 8, 77, 86, 76, 7, 50, 33, 76, 90, 54, 66, 76, 9, 333, 21]
+
 
 // TODO: IDENTIFY THE TIME COMPLEXITY OF THE FOLLOWING ARRAY METHODS
 // 1: PUSH
@@ -55,9 +56,58 @@ const findValueLinearWhile = (value) => {
 
 
 // TODO: CREATE A SIMPLE SEARCH ALGORITHM THAT USES LOGARITHMIC TIME COMPLEXITY
-const findValueLog = (value) => {
+// value = 20, 90
+    // [10,20,23,55,76,90,99]
+    // [10,20,23]  [55,76,90]
+    // find middle [3]
+    // check if middle is larger than value(20)
+    // middle in lower half [1]
+    // check if middle is larger than value(20)
+    // [10] [20,23]
+    // if value = middle => found, the value does not exists in array
+    // BINARY SEARCH ALGORITHM
+scores.sort((a, b) => a - b)
+print(scores)
+const findValueLog = (value) => {  
+    let start = 0
+    let end = scores.length - 1
+    while(start <= end){
+
+        let middle = Math.trunc( (start + end) / 2 )
+
+        if(scores [middle] === value) {
+            // return index if value equates
+            return middle
+        }else if(scores[middle] > value){
+            end = middle - 1
+        }else{
+            start = middle + 1
+        }
+    }
+    return -1
     // return the index of the value if present, return -1 if absent
 }
 
-// print(`90 is at index: ${findValueLog(90)}`)
-// print(`88 is at index: ${findValueLog(88)}`)
+// recursive approach
+const searchLogRecursive = (value, start, end) => {
+
+    if(start > end) return -1
+
+    let middle = Math.trunc((start + end) / 2)
+
+    if(scores[middle] === value) return middle
+
+    if(scores[middle] > value) {
+        searchLogRecursive(value, start, middle - 1)
+    }else{
+        searchLogRecursive(value, middle + 1, end)
+    }
+    
+    return -1
+
+}
+
+print(`990 is at index: ${findValueLog(990)}`)
+print(`88 is at index: ${findValueLog(88)}`)
+
+print(`76 is at index: ${searchLogRecursive(76, 0, scores.length - 1)}`)
